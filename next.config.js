909d9c73607a340538fddff96d170f9e6e50d493
Tanakorn Tampanya <tanakorn@fintech.co.th>
2022-06-06 23:56:26 +0700

    add wave

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'd-vote-prod.s3.amazonaws.com',
+      'd-vote-dev.s3.amazonaws.com',
+      'd-vote-dev.s3.ap-southeast-1.amazonaws.com',
+      'd-vote-prod.s3.ap-southeast-1.amazonaws.com',
+    ],
+  },
+})
---
+++ pages/gc/index.tsx
@@ -0,0 +1,13 @@
+import { Chip, TextField, Container } from "@mui/material";
+
+const GrandChasePortal = () => {
+  return (
+    <Container style={{ padding: 16 }}>
+      {/* <Box */}
+      <TextField label="set" size="small" className="text-3xl" />
+      <TextField label="stat" size="small" />
+    </Container>
+  );
+};
+
+export default GrandChasePortal;
